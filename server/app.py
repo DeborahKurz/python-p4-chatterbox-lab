0b5614f90e752837f31a943fd2d18de7f8006451
Deborah Kurz <DeborahKurz6@gmail.com>
2024-05-29 10:30:54 -0600

    POST request to messages successfull

--- server/app.py
+++ server/app.py
@@ -14,9 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+
+    
+    elif request.method == 'POST':
+        json = request.get_json()
+        new_message = Message(
+            body=json["body"],
+            username=json["username"]
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            20
+        )
+
+        return response
+
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
